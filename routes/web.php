aa70b1ef519f74f8a5fe40dc5f23ab158ab2899d
mustapha chatbi <chatbimdev@gmail.com>
2020-05-10 01:33:19 +0100

    laravel with paypal integration

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/checkout', 'checkout-page');
+Route::post('/checkout', 'PaypalController@createPayment')->name('create-payment');
+Route::get('/confirm', 'PaypalController@confirmPayment')->name('confirm-payment');
